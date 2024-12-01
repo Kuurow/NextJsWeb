9fa6c33f49b29dd7cca09d4a0da21652542aac62
Kuurow <kuuroww@gmail.com>
2024-12-01 22:49:28 +0100

    mobile fix for main page

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,10 +1,10 @@
 export default function HomePage() {
     return (
         <>
-            <div className="absolute justify-center text-center z-10 backdrop-blur-xl w-screen bg-transparent p-5 drop-shadow-sm top-1/3">
+            <div className="absolute justify-center text-center z-10 backdrop-blur-xl w-screen bg-transparent p-5 drop-shadow-sm top-0">
                 <h1 className="text-5xl text-slate-700">Work in progress...</h1>
             </div>
-            <div className="absolute justify-center text-center z-10 backdrop-blur-xl w-screen bg-transparent p-5 drop-shadow-sm bottom-1/3">
+            <div className="absolute justify-center text-center z-10 backdrop-blur-xl w-screen bg-transparent p-5 drop-shadow-sm bottom-0">
                 <h1 className="text-5xl text-slate-700">You can still check the gallery {" "}
                     <a href="/gallery" target="" className="font-semibold hover:text-fuchsia-700" rel="noreferrer"> here </a>
                 </h1>
