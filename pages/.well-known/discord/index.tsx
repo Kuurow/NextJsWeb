2912ac0e02ddaafb35f081b3337ff2afa6afc6ab
Kuurow <kuuroww@gmail.com>
2025-10-11 20:59:02 +0200

    discord test domain

--- pages/.well-known/discord/index.tsx
+++ pages/.well-known/discord/index.tsx
@@ -1,7 +1,7 @@
-class MyDocument extends Document {
-    render() {
-        return (
-            <>dh=dc864fc670b3a3c6ee1167f5d2a702cbe3f1ed88</>
-        )
-    }
+export default function Discord() {
+  return (
+    <main className="flex flex-col align-middle xl:mr-auto xl:ml-auto">
+      dh=dc864fc670b3a3c6ee1167f5d2a702cbe3f1ed88
+    </main>
+  );
 }
