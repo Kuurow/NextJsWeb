c8f0a2f6753df25a7f7ecc23cb11a2b3148c8605
Kuurow <kuuroww@gmail.com>
2025-10-11 20:56:01 +0200

    yes

--- pages/.well-known/discord/index.tsx
+++ pages/.well-known/discord/index.tsx
@@ -1,5 +1,7 @@
 class MyDocument extends Document {
     render() {
-        return ("dh=dc864fc670b3a3c6ee1167f5d2a702cbe3f1ed88")
+        return (
+            <>dh=dc864fc670b3a3c6ee1167f5d2a702cbe3f1ed88</>
+        )
     }
 }
