b9bf9766eac3abd9aab650aeb6f69719709f16fa
Kuurow <kuuroww@gmail.com>
2026-03-07 11:11:10 +0100

    fix: fixing image size to stop gallery cut behaviour on mobile

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -61,7 +61,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               >
                 <Image
                   alt="Kuu's screenshot"
-                  className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                  className="w-full h-auto transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
                   style={{ transform: "translate3d(0, 0, 0)" }}
                   placeholder={blurDataUrl ? "blur" : "empty"}
                   blurDataURL={blurDataUrl}
